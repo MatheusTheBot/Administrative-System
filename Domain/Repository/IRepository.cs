7b16515d92346ba9f6dc5dd2c173101328709037
Matheus Henrique da Lara Ferreira <matheus.henrique4474@gmail.com>
2022-02-16 18:26:17 -0300

    fixed IRepository

--- Domain/Repository/IRepository.cs
+++ Domain/Repository/IRepository.cs
@@ -1,13 +1,10 @@
-﻿using Domain.Entities;
-using Domain.Entities.Contracts;
-
-namespace Domain.Repository;
+﻿namespace Domain.Repository;
 public interface IRepository<T>
 {
     
     void Create(T entity);
-    T GetById(Guid id);
-    T GetById(int id, int id2);
+    T? GetById(Guid id);
+    T? GetById(int id, int id2);
     void Update(T entity);
     void Delete(T entity);
 }
