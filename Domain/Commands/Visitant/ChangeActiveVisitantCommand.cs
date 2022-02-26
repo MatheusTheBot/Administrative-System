b75fe7eb876b31ed03a09895d1562b2a93259839
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-02-26 16:35:32 -0300

    Added JsonConstructorAttribute and fixed Guid validation

--- Domain/Commands/Visitant/ChangeActiveVisitantCommand.cs
+++ Domain/Commands/Visitant/ChangeActiveVisitantCommand.cs
@@ -1,19 +1,24 @@
 ﻿using Domain.Commands.Contracts;
 using Flunt.Notifications;
+using System.Text.Json.Serialization;
 
 namespace Domain.Commands.Visitant;
 public class ChangeActiveVisitantCommand : Notifiable<Notification>, ICommand
 {
+    [JsonConstructor]
     public ChangeActiveVisitantCommand(bool active, Guid id)
     {
         Active = active;
         Id = id;
+
+        Validate();
     }
 
     public bool Active { get; set; }
     public Guid Id { get; set; }
     public void Validate()
     {
-        //don't need validation
+        if (Id == Guid.Empty)
+            AddNotification("Id", "Id can't be empty");
     }
 }
