848d5aa28bcfc0b2fc31600eafa340a1e7efc4df
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-02-28 18:14:47 -0300

    Added new migration

---
+++ Infra/Migrations/20220228211258_FixedPhoneLength.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class FixedPhoneLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Visitants",
+                type: "varchar(14)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(13)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Residents",
+                type: "varchar(14)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(13)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BarCode",
+                table: "Packages",
+                type: "varchar(14)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(13)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Administrators",
+                type: "varchar(14)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(13)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Visitants",
+                type: "varchar(13)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Residents",
+                type: "varchar(13)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BarCode",
+                table: "Packages",
+                type: "varchar(13)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone number",
+                table: "Administrators",
+                type: "varchar(13)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(14)");
+        }
+    }
+}
