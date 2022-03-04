c42dd363a2fab167b4ba79591c6bfe31a4c151ac
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-04 13:22:04 -0300

    Fixed Prop Type and new Migrations to fix it on Database

--- Infra/Contexts/DataContext.cs
+++ Infra/Contexts/DataContext.cs
@@ -46,7 +46,7 @@ public class DataContext : DbContext
         modelBuilder.Entity<Packages>().Property(x => x.Id).IsRequired();
         modelBuilder.Entity<Packages>().Property(x => x.ItemName).HasColumnType("varchar(150)").HasColumnName("Item name");
         modelBuilder.Entity<Packages>().Property(x => x.BarCode).HasColumnType("varchar(14)");
-        modelBuilder.Entity<Packages>().Property(x => x.Type).HasConversion<int>();
+        modelBuilder.Entity<Packages>().Property(x => x.Type).HasColumnType("varchar(6)");
         modelBuilder.Entity<Packages>().Property(x => x.Addressee).HasColumnType("varchar(250)").HasColumnName("Addressed to");
         modelBuilder.Entity<Packages>().Property(x => x.Sender).HasColumnType("varchar(150)");
         modelBuilder.Entity<Packages>().Property(x => x.SenderAddress).HasColumnType("varchar(250)").HasColumnName("Sender address");
---
+++ Infra/Migrations/20220304150320_FixedPackageType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infra.Migrations
+{
+    public partial class FixedPackageType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Packages",
+                type: "varchar(6)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Packages",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(6)");
+        }
+    }
+}
