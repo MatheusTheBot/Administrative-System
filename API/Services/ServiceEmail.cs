d2dff2a1ab168c483af66b5032fd667c97f3c464
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-04 13:15:54 -0300

    Added Services and Tools

---
+++ API/Services/ServiceEmail.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace API.Services;
+public class ServiceEmail
+{
+    public bool SendEmail(
+        string toName,
+        string toEmail,
+        string subject,
+        string body)
+    {
+        var smtpClient = new SmtpClient(Configurations.SMTP.Host, Configurations.SMTP.Port);
+
+        smtpClient.Credentials = new NetworkCredential(Configurations.SMTP.UserName, Configurations.SMTP.Password);
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        smtpClient.EnableSsl = true;
+        var mail = new MailMessage();
+
+        mail.From = new MailAddress(Configurations.MAIL.Email, Configurations.MAIL.Name);
+        mail.To.Add(new MailAddress(toEmail, toName));
+        mail.Subject = subject;
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        try
+        {
+            smtpClient.Send(mail);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ API/Tools/PasswordTool.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace API.Tools;
+public static class PasswordTool
+{
+    public static string Encript(string password)
+    {
+        var hasher = new PasswordHasher();
+        return hasher.HashPassword(password);
+    }
+    public static bool Verify(string hashedPassword,string password)
+    { 
+        var hasher = new PasswordHasher();
+        var x = hasher.VerifyHashedPassword(hashedPassword, password);
+        if(x.ToString() == "Success")
+        {
+            return true;
+        }
+        return false;
+    }
+}
