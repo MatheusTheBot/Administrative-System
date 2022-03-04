76d8c16e133a6ae76d3a5db34f5023a61e2ae4ff
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-04 13:19:59 -0300

    Added Change an Generate Password Commands to Resident, added validations

---
+++ Domain/Commands/Resident/ChangePasswordResidentCommand.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Commands.Contracts;
+using Flunt.Notifications;
+
+namespace Domain.Commands.Resident;
+public class ChangePasswordResidentCommand : Notifiable<Notification>, ICommand
+{
+    public ChangePasswordResidentCommand(string newPassword, string password, Guid id)
+    {
+        NewPassword = newPassword;
+        Password = password;
+        Id = id;
+    }
+
+    public string NewPassword { get; set; }
+    public string Password { get; set; }
+    public Guid Id { get; set; }
+
+    public void Validate()
+    {
+        //TODO
+    }
+}
---
+++ Domain/Commands/Resident/GenerateNewPasswordResidentCommand.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Commands.Contracts;
+using Flunt.Notifications;
+using System.Text.Json.Serialization;
+
+namespace Domain.Commands.Resident;
+public class GenerateNewPasswordResidentCommand : Notifiable<Notification>, ICommand
+{
+    public GenerateNewPasswordResidentCommand(string password, Guid id)
+    {
+        Password = password;
+        Id = id;
+    }
+
+    [JsonIgnore]
+    public string NewPassword { get; set; }
+    public string Password { get; set; }
+    public Guid Id { get; set; }
+
+    public void Validate()
+    {
+        //TODO
+    }
+}
