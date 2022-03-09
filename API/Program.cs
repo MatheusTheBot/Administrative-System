daa8b1768a698204a9d67c1a992eba75be0a41d6
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-09 16:23:51 -0300

    Updated Nuget Packages and added Vault key variable

--- API/Program.cs
+++ API/Program.cs
@@ -15,8 +15,12 @@ using Microsoft.IdentityModel.Tokens;
 using System.IO.Compression;
 using System.Text;
 using System.Text.Json.Serialization;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
 SetServices();
 SetAuthAndCompression();
