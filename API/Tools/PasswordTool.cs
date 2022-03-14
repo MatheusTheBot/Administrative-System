de8e4ed8e5ca3775fa5771e0cbdee79d125ebc46
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-14 16:44:59 -0300

    Refactored password encryptation/verification methods

--- API/Tools/PasswordTool.cs
+++ API/Tools/PasswordTool.cs
@@ -1,21 +1,33 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Tools;
 public static class PasswordTool
 {
     public static string Encript(string password)
     {
-        var hasher = new PasswordHasher();
-        return hasher.HashPassword(password);
+        using SHA256 hasher = SHA256.Create();
+
+        byte[] data = hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+        var result = new StringBuilder();
+
+        // Loop through each byte of the hashed data
+        // and format each one as a hexadecimal string.
+        for (int i = 0; i < data.Length; i++)
+        {
+            result.Append(data[i].ToString("x2"));
+        }
+
+        return result.ToString();
     }
     public static bool Verify(string hashedPassword,string password)
-    { 
-        var hasher = new PasswordHasher();
-        var x = hasher.VerifyHashedPassword(hashedPassword, password);
-        if(x.ToString() == "Success")
-        {
-            return true;
-        }
-        return false;
+    {
+        var PasswordToCompare = Encript(password);
+
+        // Create a StringComparer an compare the hashes.
+        StringComparer comparer = StringComparer.Ordinal;
+
+        return comparer.Compare(PasswordToCompare, hashedPassword) == 0;
     }
 }
