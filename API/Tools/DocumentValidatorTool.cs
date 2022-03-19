317eba78aa6f1d80d1fe3b9ae3364801dd73ad80
MatheusTheBot <matheus.henrique4474@gmail.com>
2022-03-19 14:18:12 -0300

    Merged both methods in a single file

--- API/Tools/DocumentValidatorTool.cs
+++ API/Tools/DocumentValidatorTool.cs
@@ -1,11 +1,66 @@
 ﻿namespace API.Tools;
-public static class CNPJValidatorTool
+public static class DocumentValidatorTool
 {
-    //Validation method reference: https://blog.dbins.com.br/como-funciona-a-logica-da-validacao-do-cnpj
-
+    //Validation method reference: https://dicasdeprogramacao.com.br/algoritmo-para-validar-cpf/
     private static readonly int[] VerificationsKeys = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
     private static readonly int[] SecondVerificationsKeys = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
-    public static bool Validate(string cnpj)
+
+    public static bool CPF(string cpf)
+    {
+        List<int> CPF = cpf.ToCharArray().Select(x => int.Parse(x.ToString())).ToList();
+
+        List<int> ValidationsDigits = new List<int>();
+        List<int> CPFDigits = new List<int>();
+        bool Response = false;
+
+        if (CPF.All(x => x == CPF.First()))
+            return Response;
+
+        for (int i = 0; i < 9; i++)
+        {
+            CPFDigits.Add(CPF[i]);
+        }
+        for (int i = 9; i <= 10; i++)
+        {
+            ValidationsDigits.Add(CPF[i]);
+        }
+
+        int Counter = 10;
+        int Result = 0;
+        foreach (int i in CPFDigits)
+        {
+            Result = Result += (i * Counter);
+            Counter--;
+        }
+
+        if ((Result * 10) % 11 == ValidationsDigits[0])
+        {
+            Response = true;
+        }
+        else
+            Response = false;
+
+        if (Response == false)
+            return Response;
+
+        int SecondCounter = 11;
+        int SecondResult = 0;
+        foreach (int i in CPFDigits)
+        {
+            SecondResult = SecondResult += (i * SecondCounter);
+            SecondCounter--;
+        }
+        SecondResult = SecondResult += ValidationsDigits[0] * SecondCounter;
+
+        if ((SecondResult * 10) % 11 == ValidationsDigits[1])
+        {
+            Response = true;
+        }
+        else
+            Response = false;
+        return Response;
+    }
+    public static bool CNPJ(string cnpj)
     {
         List<int> CNPJ = cnpj.ToCharArray().Select(x => int.Parse(x.ToString())).ToList();
 
